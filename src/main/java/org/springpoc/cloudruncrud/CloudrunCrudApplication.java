ed9a7eb05f0b9e24b93978e318b42630167b5611
Siddharth Lucky <siddharth.luckyreddy@gmail.com>
2024-09-19 13:37:23 +0530

    -initial commit

---
+++ src/main/java/org/springpoc/cloudruncrud/CloudrunCrudApplication.java
@@ -0,0 +1,13 @@
+package org.springpoc.cloudruncrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudrunCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudrunCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/springpoc/cloudruncrud/CloudrunCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springpoc.cloudruncrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudrunCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
